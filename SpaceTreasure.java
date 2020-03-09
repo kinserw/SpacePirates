3d263dfc3a1800f1f5a9da0bf4d7e7b8b86d4c1d
kinserw <57331025+kinserw@users.noreply.github.com>
2020-03-09 15:09:59 -0400

    Add files via upload

---
+++ SpaceTreasure.java
@@ -0,0 +1,64 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: SpaceTreasure.java
+ * Project name: SpacePirates
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: William Kinser, kinserw@etsu.edu
+ * Course:  CSCI 1260 277
+ * Creation Date: Mar 4, 2020
+ * ---------------------------------------------------------------------------
+ */
+
+package SpacePirates;
+
+
+/**
+ * Enter type purpose here
+ *
+ * <hr>
+ * Date created: Mar 4, 2020
+ * <hr>
+ * @author William Kinser
+ */
+public class SpaceTreasure extends SpaceObject
+{
+	/**
+	 * 
+	 */
+	private SpaceTreasureType type = SpaceTreasureType.STEEL;
+
+	/**
+	 * Constructor        
+	 *
+	 * <hr>
+	 * Date created: Mar 4, 2020 
+	 *
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public SpaceTreasure (int x, int y)
+	{
+		super (x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/**
+	 * @return type
+	 */
+	public SpaceTreasureType getTreasureType ( )
+	{
+		return type;
+	}
+
+	
+	/**
+	 * @param type the type to set
+	 */
+	public void setTreasureType (SpaceTreasureType type)
+	{
+		this.type = type;
+	}
+
+}
